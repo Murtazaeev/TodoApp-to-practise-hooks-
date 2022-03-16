f71a3f52ab23ab680a530784778a71ff06a9145d
Temur Murtazaev <temur.murtazaeveurope@gmail.com>
2022-03-16 15:36:52 +0100

    made delete button workable and checkboxes to be clickable

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,18 +1,22 @@
 import React from 'react';
-import Paper  from '@material-ui/core/Paper';
-import  List  from '@material-ui/core/List';
-import  Divider  from '@material-ui/core/Divider';
+import Paper from '@material-ui/core/Paper';
+import List from '@material-ui/core/List';
+import Divider from '@material-ui/core/Divider';
 import Todo from './Todo';
 
-function TodoList(props) {
+function TodoList({ todos, removeTodo, toggleTodo }) {
 	return (
 		<Paper>
 			<List>
-                {props.todos.map((todo) => (
-                    <>
-					<Todo task={todo.task} key={todo.id} completed={todo.completed} />
-                        <Divider />
-                    </>
+				{todos.map((todo) => (
+					<Todo
+						id={todo.id}
+						task={todo.task}
+						key={todo.id}
+						completed={todo.completed}
+						removeTodo={removeTodo}
+						toggleTodo={toggleTodo}
+					/>
 				))}
 			</List>
 		</Paper>
